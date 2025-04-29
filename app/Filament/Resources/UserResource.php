b89ff3b0f083096f6626813e42413dd7dbe21533
mcardenasp2 <marcos.cardenas@svtechnology.ec>
2025-04-28 21:27:01 -0500

    Hash

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -13,6 +13,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
 class UserResource extends Resource
